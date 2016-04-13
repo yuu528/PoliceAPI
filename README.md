ad3c97eb2e8d5fb76f630588e5368e3ac3e08693
Yuu528 <yuu528mail@gmail.com>
2016-04-13 17:15:37 +0900

    Added src, plugin.yml
    Version 1.0 uploaded. View more at README.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # PoliceAPI
 PoliceAPI is PocketMine-MP Plugin.
+
+使用法: *これはAPIです!* が警察の追加、削除、一覧表示ができます。
+
+##コマンド
+`/police`
+主なコマンドです。第一引数にdel,add,listが指定できます。
+第二引数はdel,addでプレイヤー名を指定できます。
+
+##APIとしての機能
+**APIとしての機能は今後追加していきますので追加希望があればお知らせください**
+まず、最初にコードに
+```$this->PoliceAPI->addAddon("プラグインの名前");```
+を入れてください
+
+1. getPolice() - 引数:なし
+officer.ymlのgetAll(true)の短縮です
+
+2. isPolice() - 引数:プレイヤー名
+プレイヤー名が警察かを確かめます
+
+3. setPolice() - 引数:同上
+プレイヤー名を警察にします
+
+4. removePolice() - 引数:同上
+プレイヤー名を警察から削除します
+
+5. broadcastPolice() - 引数:Text
+警察全員にブロードキャストします
+
+##Todo
+□ APIの追加
+
+END
---
+++ src/yuu528/PoliceAPI.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace yuu528;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\ConsoleCommandSender;
+
+class PoliceAPI extends PluginBase
+{
+	//System Code
+	public function onEnable()
+	{
+		$this->getServer()->getLogger()->info("[PoliceAPI] PoliceAPI Loaded!");
+		if(!file_exists($this->getDataFolder())){
+			mkdir($this->getDataFolder(), 0744, true);
+			$this->getServer()->getLogger->info("[PoliceAPI] Create Data Folder");
+		}
+
+		if(!file_exists($this->getDataFolder()."tmp")){
+			mkdir($this->getDataFolder()."tmp/", 0744, true);
+		}
+
+		$this->police = new Config($this->getDataFolder()."officer.yml", Config::YAML);
+		$this->addon = new Config($this->getDataFolder()."tmp/plugins.yml", Config::YAML);
+		//Add Configs Here
+		$this->getServer()->getLogger()->info("[PoliceAPI] Loaded Config");
+	}
+
+	public function addAddon($name){
+		if($this->addon->exists($name)){
+			return false;
+		}
+			$this->addon->set($name, "enable");
+			$this->addon->save();
+			return true;
+	}
+
+	public function removeAddon($name){
+		$this->addon->remove($name);
+		$this->addon->save();
+		return true;
+	}
+
+	public function getAddon(){
+		return $this->addon->getAll(true);
+	}
+
+	public function isAddon($name){
+		return $this->addon->exists($name);
+	}
+
+	public function onDisable(){
+		foreach ($this->addon->getAll(true) as $value) {
+			$this->removeAddon($value);
+		}
+	}
+
+	//Game Code
+	public function getPolice(){
+		return $this->police->getAll(true);
+	}
+
+	public function isPolice($name){
+		return $this->police->exists($name);
+	}
+
+	public function setPolice($name){
+		$this->police->set($name, "police");
+		$this->police->save();
+		return true;
+	}
+
+	public function removePolice($name){
+		$this->police->remove($name);
+		$this->police->save();
+		return true;
+	}
+
+	public function broadcastPolice($message){
+		foreach ($this->police->getAll() as $name) {
+			$player = $this->getServer()->getPlayer($name);
+			if(!$player instanceof Player) return;
+			$player->sendMessage($message);
+		}
+		return true;
+	}
+
+	//Command Code
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch(strtolower($command->getName())){
+			case "police":
+				if(!isset($args[0])) return false;
+				switch(strtolower($args[0])){
+					case "list":
+						$all = implode(", ", $this->getPolice());
+						$sender->sendMessage("[PoliceAPI] 警察一覧: \n{$all}");
+						return true;
+					break;
+
+					case "add":
+						if(!isset($args[1])) return false;
+
+						$this->setPolice($args[1]);
+						$this->getServer()->broadcastMessage("§a[PoliceAPI] {$sender->getName()}が{$args[1]}を警察に追加しました");
+						return true;
+					break;
+
+					case "del":
+						if(!isset($args[1])) return false;
+						if(!$this->isPolice($args[1])){
+							$sender->sendMessage("[PoliceAPI] §cそのプレイヤーは警察ではありません");
+						}
+						$this->removePolice($args[1]);
+						$this->getServer()->broadcastMessage("§a[PoliceAPI] {$sender->getName()}が{$args[1]}を警察から削除しました");
+						return true;
+					break;
+				}
+		}
+	}
+}
